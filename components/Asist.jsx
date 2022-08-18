d75e9be0d14c6761a25ae04b2ef78b87157b4020
PixiesCorp <alex.02pacheco@hotmail.com>
2022-08-18 18:13:49 -0500

    version 1.1.1

---
+++ components/Asist.jsx
@@ -0,0 +1,29 @@
+import Image from 'next/image'
+import Suit from '../public/images/suit.png'
+import Dress from '../public/images/dress.png'
+import styles from "../styles/Asist.module.css"
+import Animate from "./Animate"
+
+export default function Asist() {
+  return (
+    <div className={styles.asistContain}>
+      <h1 className={styles.asistTitle}>Codigo de Vestimenta</h1>
+      <div className={styles.asistContainImg}>
+        <span className={styles.suit}>
+          <Image src={Suit} alt="" />
+        </span>
+        <span className={styles.dress}>
+          <Image src={Dress} alt="" />
+        </span>
+      </div>
+      <h3 className={styles.typeEvent}>Formal</h3>
+      <div className={styles.info}>
+        <p>$Buzon de Dinero$</p>
+        <p>!No Niños!</p>
+      </div>
+      <h1 className={styles.confirm}>!Confirma tu asistencia!</h1>
+      <Animate />
+      <div className={styles.fill}></div>
+    </div>
+  );
+}
--- pages/asistencia.js
+++ pages/asistencia.js
@@ -1,36 +1,13 @@
-import Animate from "../components/Animate"
+import Asist from "../components/Asist"
 import NavBar from "../components/NavBar"
 import PageLayout from "../components/PageLayout"
-import styles from "../styles/Asist.module.css"
-import Image from 'next/image'
-import Suit from '../public/images/suit.png'
-import Dress from '../public/images/dress.png'
 
 export default function Asistencia (){
   return(
     <>
       <PageLayout />
       <NavBar />
-      
-      <div className={styles.asistContain}>
-        <h1 className={styles.asistTitle}>Codigo de Vestimenta</h1>
-        <div className={styles.asistContainImg}>
-          <span className={styles.suit}>
-            <Image src={Suit} alt="" />
-          </span>
-          <span className={styles.dress}>
-            <Image src={Dress} alt="" />
-          </span>
-        </div>
-        <h3 className={styles.typeEvent}>Formal</h3>
-        <div className={styles.info}>
-          <p>$Buzon de Dinero$</p>
-          <p>!No Niños!</p>
-        </div>
-        <h1 className={styles.confirm}>!Confirma tu asistencia!</h1>
-        <Animate />
-        <div className={styles.fill}></div>
-      </div>
+      <Asist />
     </>
   )
 }
